987c8b976265f257d7da1513b66d9d3ee141dec6
piotrszczodrowski <s32698@pjwstk.edu.pl>
2024-10-26 11:22:01 +0200

    variables declared before using them for better readability

--- main.cpp
+++ main.cpp
@@ -2,18 +2,19 @@
 using namespace std;
 int main()
 {
-    int guests, pizzas, pieces, guest_pieces, host_pieces;
+    int guests, pizzas;
     cout << "Podaj liczbę gosci: ";
     cin >> guests;
     cout << endl;
     cout << "Podaj liczbę placków pizzy: ";
     cin >> pizzas;
     cout << endl;
-    pieces = 8*pizzas;
+    int pieces = 8*pizzas;
     if ((pieces)<guests) {
         cout << "Za mało pizzy względem liczby gości!";
     }
     else {
+        int guest_pieces, host_pieces;
         guest_pieces = pieces/guests;
         host_pieces = pieces%guests;
         cout << "Każdy otrzyma " << guest_pieces << " kawałków (kawałki), dla organizatora zostanie " << host_pieces << " kawałków (kawałki)";
